be478796b60a6f9079c01788dc3ce87ce9ea4694
Linsa <474835552@qq.com>
2018-04-18 16:56:08 +0800

    增加热搜Fragment

---
+++ app/src/main/java/com/it/onex/onex/ui/fragment/hot/HotFragment.java
@@ -0,0 +1,27 @@
+package com.it.onex.onex.ui.fragment.hot;
+
+import android.view.View;
+
+import com.it.onex.onex.base.BaseFragment;
+
+/**
+ * Created by Linsa on 2018/4/18:16:47.
+ * des:
+ */
+
+public class HotFragment extends BaseFragment{
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initInjector() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+}
